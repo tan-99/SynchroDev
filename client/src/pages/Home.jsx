10b979aa144adc41394d3c9c607c85b26e280036
SidM77 <siddanth.manoj@gmail.com>
2023-12-25 12:37:24 +0530

    Added Blob1.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Blob1 from "../elements/Blob1";
 
 const Home = () => {
   return (
     <div>
         <h1>This is HOME</h1>
+        <Blob1/>
     </div>
   )
 }
