16df7dc78e91cfe6ee4f6e3e7f322257efb667ae
SidM77 <siddanth.manoj@gmail.com>
2023-12-26 13:31:17 +0530

    Added TextandImage2 component along iwth image and blob resized

---
+++ client/src/elements/TextImage2.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TextImage2(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default TextImage2;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Blob1 from "../elements/Blob1";
-import TextImage from "../TextImage";
+import TextImage1 from "../elements/TextImage1";
 
 const Home = () => {
   return (
-    <>
-        <div>
-            {/*<h1>This is HOME</h1>*/}
+      <>
+          <div>
+              {/*<h1>This is HOME</h1>*/}
 
-        </div>
-        <TextImage/>
-        <Blob1/>
-    </>
+          </div>
+          <TextImage1/>
+          <Blob1/>
+          <section className="spacer"></section>
+          <TextImage2/>
+          <Blob1/>
+          <section className="spacer"></section>
+      </>
   )
 }
 
