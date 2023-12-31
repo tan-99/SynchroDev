3ac0c935372efe752cbfc56a0f3516da4c6c6fb1
SidM77 <siddanth.manoj@gmail.com>
2023-12-31 17:32:02 +0530

    minor route change in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
