1bfbcc8964ea0fd82b1639b30841ab774ffe1267
SidM77 <siddanth.manoj@gmail.com>
2023-12-31 17:45:58 +0530

    added mongodb atlas uri

--- server/index.js
+++ server/index.js
@@ -9,3 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(8000, () => console.log("Backend Server Running on port 8000"));
+
+const uri =
+  "mongodb://siddanthmanoj:siddanthmanoj@ac-woab2wp-shard-00-00.yurdnnj.mongodb.net:27017,ac-woab2wp-shard-00-01.yurdnnj.mongodb.net:27017,ac-woab2wp-shard-00-02.yurdnnj.mongodb.net:27017/?ssl=true&replicaSet=atlas-xgckfc-shard-0&authSource=admin&retryWrites=true&w=majority";
